6cab271c29edd7e4a12dac8ba40388d7512a038e
Grissia <wasd.lhe@gmail.com>
2026-03-31 09:33:20 +0800

    add: docs/reverse2/switch

---
+++ docs/reverse2/switch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int num = 10;
+	switch(num){
+		case 1:
+			printf("It's 1\n");
+			break;
+		case 10:
+			printf("It's 10\n");
+			break;
+		case 100:
+			printf("It's 100\n");
+			break;
+		case 1000:
+			printf("It's 1000\n");
+			break;
+	}
+        return 0;
+}
